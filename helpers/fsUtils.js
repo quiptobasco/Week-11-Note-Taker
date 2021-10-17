26f5ef4a31c689a56dedae5c08d2984e1aa6a03a
quiptobasco <quiptobasco@gmail.com>
2021-10-16 18:10:45 -0700

    Initial commit

---
+++ helpers/fsUtils.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 2), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+const readAndDelete = (file, id) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(id);
+            const parsedData = JSON.parse(data);
+            var filteredData = parsedData.filter(e => e.id !== id);
+            writeToFile(file, filteredData);
+        }
+    })
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
