e496d23a9ea559b9baced5fa2a0a60d5e106d848
quiptobasco <quiptobasco@gmail.com>
2021-10-19 17:42:13 -0700

    added comments to code

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -3,11 +3,13 @@ const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
 
+// function to write to a file a JSON string object
 const writeToFile = (destination, content) =>
     fs.writeFile(destination, JSON.stringify(content, null, 2), (err) =>
         err ? console.error(err) : console.info(`\nData written to ${destination}`)
 );
 
+// function to read a file, then add new content and write to the database
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
@@ -20,6 +22,7 @@ const readAndAppend = (content, file) => {
     });
 };
 
+// function to read a file, filter the data to not include any entry with a matching id
 const readAndDelete = (file, id) => {
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
